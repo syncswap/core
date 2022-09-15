705578361127fd9a2e924d202925f52efbd99463
nakato <94997114+0xnakato@users.noreply.github.com>
2022-09-15 02:38:45 +0000

    diff univ2

--- contracts/SyncSwapERC20.sol
+++ contracts/SyncSwapERC20.sol
@@ -1,38 +1,37 @@
-pragma solidity =0.5.17;
+pragma solidity =0.5.16;
 
-import './interfaces/ISyncSwapERC20.sol';
+import './interfaces/IUniswapV2ERC20.sol';
 import './libraries/SafeMath.sol';
-import './libraries/SignatureChecker.sol';
 
-contract SyncSwapERC20 is ISyncSwapERC20 {
+contract UniswapV2ERC20 is IUniswapV2ERC20 {
     using SafeMath for uint;
 
-    string public name;
-    string public symbol;
+    string public constant name = 'Uniswap V2';
+    string public constant symbol = 'UNI-V2';
     uint8 public constant decimals = 18;
-
-    uint public totalSupply;
+    uint  public totalSupply;
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
 
-    bytes32 private DOMAIN_SEPARATOR;
+    bytes32 public DOMAIN_SEPARATOR;
     // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
-    bytes32 private constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
+    bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
     mapping(address => uint) public nonces;
 
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
+
     constructor() public {
+        uint chainId;
+        assembly {
+            chainId := chainid
+        }
         DOMAIN_SEPARATOR = keccak256(
             abi.encode(
-                // keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
-                0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f,
-
-                // keccak256(bytes('SyncSwap SLP Token')),
-                0xf217daa25f50651e072a913278858c2bfea9576ad0ab9d0a2cd31744cea22cbe,
-
-                // keccak256(bytes('1')),
-                0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6,
-
-                280, // SyncSwap - hardcode chain id
+                keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
+                keccak256(bytes(name)),
+                keccak256(bytes('1')),
+                chainId,
                 address(this)
             )
         );
@@ -55,55 +54,41 @@ contract SyncSwapERC20 is ISyncSwapERC20 {
         emit Approval(owner, spender, value);
     }
 
+    function _transfer(address from, address to, uint value) private {
+        balanceOf[from] = balanceOf[from].sub(value);
+        balanceOf[to] = balanceOf[to].add(value);
+        emit Transfer(from, to, value);
+    }
+
     function approve(address spender, uint value) external returns (bool) {
         _approve(msg.sender, spender, value);
         return true;
     }
 
     function transfer(address to, uint value) external returns (bool) {
-        balanceOf[msg.sender] = balanceOf[msg.sender].sub(value);
-        balanceOf[to] = balanceOf[to].add(value);
-        emit Transfer(msg.sender, to, value);
+        _transfer(msg.sender, to, value);
         return true;
     }
 
     function transferFrom(address from, address to, uint value) external returns (bool) {
-        if (from != msg.sender) {
-            uint _allowance = allowance[from][msg.sender];
-            if (_allowance != uint(-1)) {
-                allowance[from][msg.sender] = _allowance.sub(value);
-            }
+        if (allowance[from][msg.sender] != uint(-1)) {
+            allowance[from][msg.sender] = allowance[from][msg.sender].sub(value);
         }
-        balanceOf[from] = balanceOf[from].sub(value);
-        balanceOf[to] = balanceOf[to].add(value);
-        emit Transfer(from, to, value);
+        _transfer(from, to, value);
         return true;
     }
 
     function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external {
-        require(deadline >= block.timestamp, 'X');
-        bytes32 hash = keccak256(
+        require(deadline >= block.timestamp, 'UniswapV2: EXPIRED');
+        bytes32 digest = keccak256(
             abi.encodePacked(
                 '\x19\x01',
                 DOMAIN_SEPARATOR,
                 keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner]++, deadline))
             )
         );
-        address recoveredAddress = ecrecover(hash, v, r, s);
-        require(recoveredAddress != address(0) && recoveredAddress == owner, 'S');
-        _approve(owner, spender, value);
-    }
-
-    function permit2(address owner, address spender, uint value, uint deadline, bytes calldata signature) external {
-        require(deadline >= block.timestamp, 'X');
-        bytes32 hash = keccak256(
-            abi.encodePacked(
-                '\x19\x01',
-                DOMAIN_SEPARATOR,
-                keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner]++, deadline))
-            )
-        );
-        require(SignatureChecker.checkSignature(owner, hash, signature), 'S');
+        address recoveredAddress = ecrecover(digest, v, r, s);
+        require(recoveredAddress != address(0) && recoveredAddress == owner, 'UniswapV2: INVALID_SIGNATURE');
         _approve(owner, spender, value);
     }
 }
